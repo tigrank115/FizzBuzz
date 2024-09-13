75c4ad7998c26fba790655b3ccac659496a08195
tigrank115 <tigran.shatmets@gmail.com>
2024-09-13 15:33:16 -0400

    Introduce Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Call FizzBuzz on 375, which should be
+        // divisible by both 5 and 3.
+        FizzBuzz.doFizzBuzz(375);
+    }
+}
